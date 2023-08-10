328ac80ad09d5c663824d2e95312497b3680a38e
Pablo Strapasson <pablostrapasson98@gmail.com>
2023-08-10 20:07:35 -0300

    node + fetch json

--- src/app.js
+++ src/app.js
@@ -8,39 +8,15 @@ router.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname + "/views/index.html"))
 })
 
-app.use('/', router);
+router.get("/create_link", (req, res) => {
+    res.sendFile(path.join(__dirname + "/views/create_link.html"))
+})
+
 app.listen(port);
 
-/*function onResponse(response){
-    console.log(response.status);
-    response.json().then(json => console.log(json));
-}
+app.set('view engine', 'ejs');
 
-function onError(error){
-    console.log("erro " + error);
-}
-
-fetch('../../public/links.json').then(onResponse, onError)
-
-const urlParams = new URLSearchParams(window.location.search);
-
-var link_title = urlParams.get("title");
-var link_url = urlParams.get("link");
-
-if(link_title !== null && link_url !== null){
-    createLinkItem(link_title, link_url);
-}
-
-function createLinkItem(title, link){
-    var link_list = document.getElementById('links');
-
-    var a_link = document.createElement("A");
-    a_link.href = link;
-    a_link.target = "_blank";
-    
-    var p_link = document.createElement("P");
-    p_link.textContent = title;
-
-    a_link.appendChild(p_link);
-    link_list.appendChild(a_link);
-}*/
+app.use('/', router);
+app.use(express.static(path.join(__dirname, 'styles')));
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, 'scripts')));
